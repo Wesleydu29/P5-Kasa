6fb23fe5f8bb1fbf007789377da449c95ffe47b1
Wesley Goarant <Wesleydu29860@gmail.com>
2024-08-28 11:10:33 +0200

    nouvelle mise à jour du carousel

--- src/components/CarouselBanner.jsx
+++ src/components/CarouselBanner.jsx
@@ -5,13 +5,13 @@ function CarouselBanner({ images }) {
     const [currentIndex, setCurrentIndex] = useState(0);
 
     const handlePrevClick = () => {
-        setCurrentIndex((prevIndex) => 
+        setCurrentIndex((prevIndex) =>
             prevIndex === 0 ? images.length - 1 : prevIndex - 1
         );
     };
 
     const handleNextClick = () => {
-        setCurrentIndex((prevIndex) => 
+        setCurrentIndex((prevIndex) =>
             prevIndex === images.length - 1 ? 0 : prevIndex + 1
         );
     };
@@ -19,11 +19,17 @@ function CarouselBanner({ images }) {
     return (
         <div className="carousel-banner">
             <img src={images[currentIndex]} alt="Apartment view" className="carousel-image" />
-            <button className="carousel-button prev" onClick={handlePrevClick}>‹</button>
-            <button className="carousel-button next" onClick={handleNextClick}>›</button>
-            <div className="image-counter">
-                {currentIndex + 1} / {images.length}
-            </div>
+            
+            {/* affiche les boutons et le compteur uniquement s'il y a plus d'une image */}
+            {images.length > 1 && (
+                <>
+                    <button className="carousel-button prev" onClick={handlePrevClick}>‹</button>
+                    <button className="carousel-button next" onClick={handleNextClick}>›</button>
+                    <div className="image-counter">
+                        {currentIndex + 1} / {images.length}
+                    </div>
+                </>
+            )}
         </div>
     );
 }
